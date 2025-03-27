16eb8a0b949da7470ddaa228b97f980f4d0c7f23
viettranni <vttranviett@gmail.com>
2025-03-27 17:57:44 +0200

    Fixing

--- src/main/java/com/example/fuelcalculation/HelloController.java
+++ src/main/java/com/example/fuelcalculation/HelloController.java
@@ -34,31 +34,26 @@ public class HelloController {
     public Label localTime;
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText(rb.getString("hello"));
+        welcomeText.setText(resource.getString("hello"));
     }
 
-    ResourceBundle rb;
+    ResourceBundle resource;
     public void onCalculateClick(ActionEvent actionEvent) {
         if (txtDistance == null || txtFuel == null) {
             System.err.println("TextFields are empty");
             return;
         }
         try {
-// Parse distance and height values
             double distance = Double.parseDouble(txtDistance.getText());
             double fuel = Double.parseDouble(txtFuel.getText());
-// Calculate BMI using the formula: BMI = distance / (height * height)
             double fuel_needed = fuel * (distance / 100);
             System.out.println("Raw BMI: " + fuel_needed);
-// Format the BMI value to 2 decimal places
             DecimalFormat df = new DecimalFormat("#0.00");
             String fuel_String = df.format(fuel_needed);
             System.out.println("Formatted BMI: " + fuel_String);
-// Directly concatenate the BMI value to the result message
-            lblResult.setText(String.format(rb.getString("result.label"), fuel_String));
+            lblResult.setText(String.format(resource.getString("result.label"), fuel_String));
         } catch (NumberFormatException e) {
-// Handle invalid input
-            lblResult.setText(rb.getString("invalid.input"));
+            lblResult.setText(resource.getString("invalid.input"));
         }
     }
 
@@ -80,23 +75,25 @@ public class HelloController {
 
     public void initialize() {
         Locale locale = new Locale("en", "US");
-        rb = ResourceBundle.getBundle("messages", locale);
+        resource = ResourceBundle.getBundle("messages", locale);
         setLanguage(locale);
         getLocaleTime(locale);
     }
 
+    // Returning Locale Time
     private void getLocaleTime(Locale locale) {
         LocalDate currentDate = LocalDate.now();
         localTime.setText(String.valueOf(currentDate));
     }
 
+    // Setting the Locale
     private void setLanguage(Locale locale) {
         lblResult.setText("");
         try {
-            rb = ResourceBundle.getBundle("messages", locale);
-            lblDistance.setText(rb.getString("distance.label"));
-            lblFuel.setText(rb.getString("fuel.label"));
-            btnCalculate.setText(rb.getString("calculate.button"));
+            resource = ResourceBundle.getBundle("messages", locale);
+            lblDistance.setText(resource.getString("distance.label"));
+            lblFuel.setText(resource.getString("fuel.label"));
+            btnCalculate.setText(resource.getString("calculate.button"));
         }catch(MissingResourceException e) {
             e.printStackTrace();
             lblResult.setText("Error loading resources error");
